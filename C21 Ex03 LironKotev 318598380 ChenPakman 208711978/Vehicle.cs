7ab4024c909bba57ed2917e9b0b70b645d0f66d7
Liron aviv Kotev <lironkt@mta.ac.il>
2021-08-12 16:41:41 +0300

    New

---
+++ C21 Ex03 LironKotev 318598380 ChenPakman 208711978/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Vehicle
+    {
+        private string m_ModelName;
+        private string m_LicenseNumber;
+        private float m_EnergyPercentage;
+        protected List<Wheel> m_ListOfWheels;
+
+
+
+    }
+}
---
+++ C21 Ex03 LironKotev 318598380 ChenPakman 208711978/Wheel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class Wheel
+    {
+        private string m_Manufacturer;
+        private float m_CurrentAirPressure;
+        private readonly float  r_MaximumAirPressureSetByManufacturer;
+
+        public Wheel(string i_Manufacturer, float i_CurrentAirPressure, float i_MaximumAirPressureSetByManufacturer)
+        {
+            m_Manufacturer = i_Manufacturer;
+            m_CurrentAirPressure = i_CurrentAirPressure;
+            r_MaximumAirPressureSetByManufacturer = i_MaximumAirPressureSetByManufacturer;
+        }
+        public void InflationAction(float i_AirPressureToAdd)
+        {
+            float newAirPressure = i_AirPressureToAdd + m_CurrentAirPressure;
+
+            if(newAirPressure> r_MaximumAirPressureSetByManufacturer)
+            {
+                //throw 
+            }
+            else
+            {
+                m_CurrentAirPressure = newAirPressure;
+            }
+        }
+
+
+
+
+    }
+}
